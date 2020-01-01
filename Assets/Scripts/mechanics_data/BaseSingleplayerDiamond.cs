c6764b372cd657f76656cca77135c62b038ba228
crassSandwich <christopherjanidlo@gmail.com>
2019-12-31 23:37:17 -0500

    Implement starting note diamond

--- Assets/Scripts/mechanics_data/BaseSingleplayerDiamond.cs
+++ Assets/Scripts/mechanics_data/BaseSingleplayerDiamond.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExampleNoteDiamond : NoteDiamond
+public class BaseSingleplayerDiamond : NoteDiamond
 {
+    const float MULT = 1.5f;
+    const int ADD = 2;
+
 	protected override InputDirectionBox<Note> initializeNotes()
 	{
         return new InputDirectionBox<Note>
@@ -17,14 +20,17 @@ public class ExampleNoteDiamond : NoteDiamond
         };
 	}
 
+    // main: bpm up
+    // meta: multiply
 	class Up : Note
 	{
 		protected override NoteData data => new NoteData
         {
             Direction = InputDirection.Up,
-            InitialVector = new EffectVector(1, false),
-            Color = Color.white,
-            MetaEffectDescription = "increases the effect of the spell by 1",
+            InitialVector = new EffectVector(3, false),
+            Color = Color.red, // TODO: specify red
+            MetaEffectDescription = $"multiplies the effect of the spell by {MULT}",
+            // TODO:
             MainCombos = new ComboData
             {
                 Up = true,
@@ -32,6 +38,7 @@ public class ExampleNoteDiamond : NoteDiamond
                 Down = true,
                 Right = true
             },
+            // TODO:
             MetaCombos = new MetaComboData
             {
                 Up = new ComboData
@@ -67,28 +74,31 @@ public class ExampleNoteDiamond : NoteDiamond
 
 		public override string DescribeMainEffect (EffectVector vector)
 		{
-            return $"Adds {(int) vector.Power} cards to your track.";
+            return $"increases your track's BPM by {((int) vector.Power) * Track.BPM_PER_BSTEP}";
 		}
 
 		public override void MainEffect (Track input, EffectVector vector)
 		{
-            input.SpawnCards((int) vector.Power);
+            input.BSteps += (int) vector.Power;
 		}
 
 		public override EffectVector MetaEffect (EffectVector vector)
 		{
-            return vector + 1;
+            return vector * MULT;
 		}
 	}
 
+    // main: cards down
+    // meta: subtract
 	class Left : Note
 	{
 		protected override NoteData data => new NoteData
         {
             Direction = InputDirection.Left,
-            InitialVector = new EffectVector(1, false),
-            Color = Color.white,
-            MetaEffectDescription = "increases the effect of the spell by 1",
+            InitialVector = new EffectVector(2, false),
+            Color = Color.white, // TODO: placeholder. want a blue-tinted white
+            MetaEffectDescription = $"decreases the effect of the spell by {ADD}",
+            // TODO:
             MainCombos = new ComboData
             {
                 Up = true,
@@ -96,6 +106,7 @@ public class ExampleNoteDiamond : NoteDiamond
                 Down = true,
                 Right = true
             },
+            // TODO:
             MetaCombos = new MetaComboData
             {
                 Up = new ComboData
@@ -131,28 +142,31 @@ public class ExampleNoteDiamond : NoteDiamond
 
 		public override string DescribeMainEffect (EffectVector vector)
 		{
-            return $"Adds {(int) vector.Power} cards to your track.";
+            return $"clears {(int) vector.Power} cards from your track";
 		}
 
 		public override void MainEffect (Track input, EffectVector vector)
 		{
-            input.SpawnCards((int) vector.Power);
+            input.ClearCards((int) vector.Power);
 		}
 
 		public override EffectVector MetaEffect (EffectVector vector)
 		{
-            return vector + 1;
+            return vector - ADD;
 		}
 	}
 
+    // main: card spawn rate down
+    // meta: negadivide
 	class Down : Note
 	{
 		protected override NoteData data => new NoteData
         {
             Direction = InputDirection.Down,
             InitialVector = new EffectVector(1, false),
-            Color = Color.white,
-            MetaEffectDescription = "increases the effect of the spell by 1",
+            Color = Color.blue, // TODO: specify blue
+            MetaEffectDescription = $"flips the polarity of the spell and multiplies its effect by {1/MULT}",
+            // TODO:
             MainCombos = new ComboData
             {
                 Up = true,
@@ -160,6 +174,7 @@ public class ExampleNoteDiamond : NoteDiamond
                 Down = true,
                 Right = true
             },
+            // TODO:
             MetaCombos = new MetaComboData
             {
                 Up = new ComboData
@@ -195,28 +210,32 @@ public class ExampleNoteDiamond : NoteDiamond
 
 		public override string DescribeMainEffect (EffectVector vector)
 		{
-            return $"Adds {(int) vector.Power} cards to your track.";
+            int effect = (int) vector.Power;
+            return $"increases the time between card spawns by {effect} beat{(effect > 1 ? "s" : "")}";
 		}
 
 		public override void MainEffect (Track input, EffectVector vector)
 		{
-            input.SpawnCards((int) vector.Power);
+            input.BeatsPerCard += (int) vector.Power;
 		}
 
 		public override EffectVector MetaEffect (EffectVector vector)
 		{
-            return vector + 1;
+            return -vector / MULT;
 		}
 	}
 
+    // main: cards up
+    // meta: add
 	class Right : Note
 	{
 		protected override NoteData data => new NoteData
         {
             Direction = InputDirection.Right,
-            InitialVector = new EffectVector(1, false),
-            Color = Color.white,
-            MetaEffectDescription = "increases the effect of the spell by 1",
+            InitialVector = new EffectVector(2, false),
+            Color = Color.magenta, // TODO: placeholder. want an orange
+            MetaEffectDescription = $"increases the effect of the spell by {ADD}",
+            // TODO:
             MainCombos = new ComboData
             {
                 Up = true,
@@ -224,6 +243,7 @@ public class ExampleNoteDiamond : NoteDiamond
                 Down = true,
                 Right = true
             },
+            // TODO:
             MetaCombos = new MetaComboData
             {
                 Up = new ComboData
@@ -259,7 +279,7 @@ public class ExampleNoteDiamond : NoteDiamond
 
 		public override string DescribeMainEffect (EffectVector vector)
 		{
-            return $"Adds {(int) vector.Power} cards to your track.";
+            return $"adds {(int) vector.Power} cards to your track";
 		}
 
 		public override void MainEffect (Track input, EffectVector vector)
@@ -269,7 +289,7 @@ public class ExampleNoteDiamond : NoteDiamond
 
 		public override EffectVector MetaEffect (EffectVector vector)
 		{
-            return vector + 1;
+            return vector + ADD;
 		}
 	}
 }
