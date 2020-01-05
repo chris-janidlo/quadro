ef6d0425d9b5628a52cbd461d962877fd23f8571
crassSandwich <christopherjanidlo@gmail.com>
2020-01-05 15:10:29 -0500

    Add visual for combo counter

---
+++ Assets/Scripts/ComboCountVisuals.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ComboCountVisuals : MonoBehaviour
+{
+    public ADriver Driver;
+    public TextMeshProUGUI Text;
+
+    void Update ()
+    {
+        Text.text = Driver.State.Rhythm.ComboCounter.ToString();
+    }
+}
