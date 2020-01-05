1b178efc49cc554ad35b283e995952fa8ec8dda1
crassSandwich <christopherjanidlo@gmail.com>
2020-01-04 21:30:13 -0500

    Spell descriptions

---
+++ Assets/Scripts/SpellDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpellDescriptor : MonoBehaviour
+{
+    public ADriver Driver;
+    public TextMeshProUGUI Text;
+
+    void Update ()
+    {
+        string text = "";
+
+        if (Driver.State.Rhythm.ComboCounter > 0 && Driver.State.CurrentSpell != null)
+        {
+            text = Driver.State.CurrentSpell.Description;
+        }
+
+        Text.text = text;
+    }
+}
