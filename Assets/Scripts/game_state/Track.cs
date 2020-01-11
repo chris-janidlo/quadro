4f4eff3e25cab8b81eb614ae1fe96e998baa8c29
crassSandwich <christopherjanidlo@gmail.com>
2020-01-11 11:13:19 -0500

    Revert STARTING_BSTEPS change

--- Assets/Scripts/game_state/Track.cs
+++ Assets/Scripts/game_state/Track.cs
@@ -19,7 +19,7 @@ public class Track
     public const int BPM_PER_BSTEP = 5;
     public const int MIN_BSTEPS = 12;
     public const int MAX_BSTEPS = 40;
-    public const int STARTING_BSTEPS = 40;
+    public const int STARTING_BSTEPS = 16;
 
     public const int MAX_BEATS_PER_CARD = 16;
     public const int STARTING_BEATS_PER_CARD = 8;
