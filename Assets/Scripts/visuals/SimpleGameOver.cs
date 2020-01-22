0a658cc5a6627f3ae89e2ccc95efc743e2af539c
crassSandwich <christopherjanidlo@gmail.com>
2020-01-21 20:15:02 -0500

    Super basic game over loop

---
+++ Assets/Scripts/visuals/SimpleGameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleGameOver : MonoBehaviour
+{
+    public ADriver Driver;
+
+    void Update ()
+    {
+        if (Driver.State.Track.Dead)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+}
